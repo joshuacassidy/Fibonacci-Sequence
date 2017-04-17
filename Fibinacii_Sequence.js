1dda86f9c3f10783f3919e0ce05547547084b8f4
joshuacassidy <Joshcassidy79@gmail.com>
2017-04-17 13:59:57 +0100

    Cleaned up Code and Created a Function to calculate next Term of Sequence

--- Fibinacii_Sequence.js
+++ Fibinacii_Sequence.js
@@ -1,19 +1,17 @@
 var x = 0;
 var y = 1;
-function seq(x,y){
+function Enumeration(){
+  sequenceOfNumbers.push(x+y);
+  y =x+y;
+  x = y-x;
+}
+function Seq(x,y){
 
   sequenceOfNumbers = [];
   numbersInSequence = parseInt(prompt("How many terms would you like to be displayed of the Fibinacii Sequence"));
   for(i = 0; i < numbersInSequence; i++){
-    if(i>1){
-      sequenceOfNumbers.push(x+y);
-      y =x+y;
-      x = y-x;
-    }
-    else{
-      sequenceOfNumbers.push(i);
-    }
+    i>1 ? Enumeration(x,y) : sequenceOfNumbers.push(i);
 }
 }
-seq(x,y);
+Seq(x,y);
 alert(`The first ${numbersInSequence} numbers in the Fibinacii sequence are ${sequenceOfNumbers}`);
