d57a83e5fbf468bf41b051ee465887d2c593e7d4
joshuacassidy <Joshcassidy79@gmail.com>
2017-04-28 18:13:51 +0100

    removing unneccessary variables

--- Fibinacii_Sequence.js
+++ Fibinacii_Sequence.js
@@ -1,17 +1,15 @@
-var x = 0;
-var y = 1;
-function Enumeration(){
-  sequenceOfNumbers.push(x+y);
-  y =x+y;
-  x = y-x;
-}
-function Seq(x,y){
+(function (x,y){
 
-  sequenceOfNumbers = [];
-  numbersInSequence = parseInt(prompt("How many terms would you like to be displayed of the Fibinacii Sequence"));
+  function Enumeration(){
+    sequenceOfNumbers.push(x+y);
+    y =x+y;
+    x = y-x;
+  }
+
+  var sequenceOfNumbers = [];
+  var numbersInSequence = parseInt(prompt("How many terms would you like to be displayed of the Fibinacii Sequence"));
   for(i = 0; i < numbersInSequence; i++){
     i>1 ? Enumeration(x,y) : sequenceOfNumbers.push(i);
-}
-}
-Seq(x,y);
-alert(`The first ${numbersInSequence} numbers in the Fibinacii sequence are ${sequenceOfNumbers}`);
+  }
+  alert(`The first ${numbersInSequence} terms in the Fibinacii sequence are ${sequenceOfNumbers}`);
+})(0,1);
