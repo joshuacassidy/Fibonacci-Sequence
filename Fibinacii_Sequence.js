2eaf5b3cdf9dcf442b5b48c882c6eff5bc8f271d
joshuacassidy <Joshcassidy79@gmail.com>
2017-04-29 15:29:04 +0100

    adding the sequenceOfNumbers variable as a parameter

--- Fibinacii_Sequence.js
+++ Fibinacii_Sequence.js
@@ -1,15 +1,14 @@
-(function (x,y){
+(function (x,y,sequenceOfNumbers){
 
   function Enumeration(){
     sequenceOfNumbers.push(x+y);
-    y =x+y;
+    y = x+y;
     x = y-x;
   }
 
-  var sequenceOfNumbers = [];
   var numbersInSequence = parseInt(prompt("How many terms would you like to be displayed of the Fibinacii Sequence"));
   for(i = 0; i < numbersInSequence; i++){
     i>1 ? Enumeration(x,y) : sequenceOfNumbers.push(i);
   }
   alert(`The first ${numbersInSequence} terms in the Fibinacii sequence are ${sequenceOfNumbers}`);
-})(0,1);
+})(0,1,[]);
